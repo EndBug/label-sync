1037f7b40204b2b82c9445b72ab2caa1df02c8cb
Simon Lepel <simbo@simbo.de>
2022-08-26 05:33:46 +0200

    reset quotes

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ let configSource!: 'list' | 'repo'
       color: label.color.replace(/^#/, '')
     }))
 
-    startGroup(`Syncing labels...`)
+    startGroup('Syncing labels...')
     const options: Options = {
       accessToken: getInput('token'),
       endpoint: process.env.GITHUB_API_URL?.replace(/^https?:\/\//, ''),
